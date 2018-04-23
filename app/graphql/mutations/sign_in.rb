2574e16810d7103c627467f649e019fdeed547ff
Patrício dos Santos <eu@patricio.me>
2018-04-23 21:58:36 +0100

    Sign in mutation

---
+++ app/graphql/mutations/sign_in.rb
@@ -0,0 +1,19 @@
+class Mutations::SignIn < GraphQL::Function
+  argument :email, !Types::AuthProviderEmailInput
+
+  type Types::AuthenticateType
+
+  def call(obj, args, ctx)
+    input = args[:email]
+    return unless input
+
+    user = User.find_by(email: input[:email])
+    return unless user
+    return unless user.authenticate(input[:password])
+
+    OpenStruct.new({
+      token: AuthToken.sign(user),
+      user: user
+    })
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,4 +2,5 @@ Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   field :createUser, function: Mutations::CreateUser.new
+  field :signIn, function: Mutations::SignIn.new
 end
